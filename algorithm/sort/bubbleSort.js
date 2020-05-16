4cba126adf801b290ff5e572a02714d9fe6a430f
Naveen <naveen@Naveens-MacBook-Air.local>
2020-05-16 12:52:15 +0530

    bubble sort added

---
+++ algorithm/sort/bubbleSort.js
@@ -0,0 +1,60 @@
+/**
+ * 
+ * the navie approach without the optimization code, the main reason to say it is not optimized is because we 
+ * compare it with undefined and run the loop even if the array is almost sorted or sorted 
+ */
+
+/**
+ * 
+ * @param {Array} arr
+ * 
+ */
+
+ function bubbleSortWithoutOptimization(arr){
+    for(let i=0;i<arr.length;i++){
+        for(j=0;j<arr.legth;j++){
+            //swap the element on comparison
+            if(arr[j]> arr[j+1]){
+                let temp= arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+
+ }
+
+
+
+ bubbleSortWithoutOptimization([2,5,8,3,4]) // should return [2,3,4,5,8] 
+
+
+
+
+/**
+ * 
+ * @param {Array} arr
+ * @returns {Array} //sorted array
+ * 
+ * below function helps to sort the array and we avoid the extra loops if the aray is alomst sorted with noSwap variable,
+ * Also on each loop we reduce the comparision. 
+ */
+function bubbleSortWithOptimization(arr){
+    let noswap;
+    for(let i= arr.length; i>0; i--){
+        noswap = true;
+        for(let j=0;j<i-1;j++){
+            if(arr[j]>arr[j+1]){
+                let temp = arr[j];
+                arr[j]= arr[j+1];
+                arr[j+1] = temp;
+                noswap = false;
+            }
+           
+        }
+         if(noswap) break;
+    }
+    return arr;
+}
+bubbleSortWithOptimization([2,3,7,4,5]);
