38a6a8d4529450c85977bb4247d75e33b836301a
navinnarshetty <navinnarshetty@gmail.com>
2020-06-07 19:03:49 +0530

    reverse and print method added for singly linked list

--- data_structure/SinglyLinkedList/singlyLinkedList.js
+++ data_structure/SinglyLinkedList/singlyLinkedList.js
@@ -141,5 +141,33 @@ class SinglyLinkedList {
         this.length--;
         return true;
     }
+
+    // reverse the list 
+    reverse(){
+        var node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+
+        var next;
+        var prev = null;
+        for(var i=0;i<this.length;i++){
+            next = node.next;
+            node.next= prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+    }
+
+    // for the debugging purpose
+    print() {
+        var arr=[];
+        var current = this.head;
+        while (current) {
+            arr.push(current.val);
+            current = current.next;
+        }
+        return arr;
+    }
 }
 
