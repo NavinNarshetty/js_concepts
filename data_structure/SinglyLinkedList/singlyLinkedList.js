90768d6b9cc93b8d9e080c6112e1860e82d9cbf8
navinnarshetty <navinnarshetty@gmail.com>
2020-06-07 19:42:46 +0530

    added removeDuplicates method on singly linked list only if the list is sorted

--- data_structure/SinglyLinkedList/singlyLinkedList.js
+++ data_structure/SinglyLinkedList/singlyLinkedList.js
@@ -171,3 +171,18 @@ class SinglyLinkedList {
     }
 }
 
+
+
+// Remove Duplicate from the sorted linked list
+function removeDuplicates(head){
+    var node = head;
+    while (head.next != null) {
+        if(head.val === head.next.val){
+            head.next = head.next.next;
+        }else{
+            head = head.next;
+        }
+    }
+    return node;
+}
+
